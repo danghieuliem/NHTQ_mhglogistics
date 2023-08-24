bf156605084b66dc2b15d2b056aeaf8ee3934ef3
sinhvusinh1997 <sinhvusinh1997@gmail.com>
2023-08-24 22:27:00 +0700

    add new login page

--- src/components/screens/auth/RegisterForm.tsx
+++ src/components/screens/auth/RegisterForm.tsx
@@ -1,4 +1,3 @@
-import { Card, Modal } from "antd";
 // import {signIn} from "next-auth/react";
 import Cookie from "js-cookie";
 import React, { useEffect, useState } from "react";
@@ -10,8 +9,12 @@ import { config } from "~/configs";
 import { setRouter, updateUser, useAppDispatch } from "~/store";
 import { _format } from "~/utils";
 import { EUnique, checkUnique, createComplain } from "./method";
+import { useRouter } from "next/router";
 
-const RegisterForm = ({ visible, setOpenModal }) => {
+const aLink =
+  "cursor-pointer text-main hover:text-sec transition-all duration-300";
+
+const RegisterForm = ({ handleOpen }) => {
   const dispatch = useAppDispatch();
 
   const { handleSubmit, control, watch, reset } = useForm<TUserRegister>({
@@ -29,6 +32,7 @@ const RegisterForm = ({ visible, setOpenModal }) => {
   const [psIcon, setPsIcon] = useState(false);
   const [cpsIcon, setCpsIcon] = useState(false);
   const [loading, setLoading] = useState(false);
+  const router = useRouter();
 
   useEffect(() => {
     reset({
@@ -39,7 +43,7 @@ const RegisterForm = ({ visible, setOpenModal }) => {
       Phone: "",
       FullName: "",
     });
-  }, [visible]);
+  }, []);
 
   const { mutate, isLoading } = useMutation(
     (data: TUserRegister) => authenticate.register(data),
@@ -67,8 +71,8 @@ const RegisterForm = ({ visible, setOpenModal }) => {
                 Token: token,
               })
             );
-            setOpenModal("");
             setLoading(false);
+            router.push('/user/');
             dispatch(setRouter(user.UserGroupId));
           })
           .catch(() => console.log("error to fetching user by id!"));
@@ -92,232 +96,206 @@ const RegisterForm = ({ visible, setOpenModal }) => {
   };
 
   return (
-    <Modal visible={visible} footer={false} closeIcon={true} closable={false}>
-      <div className="authContainer">
-        <Card
-          className="!m-[-10px]"
-          extra={
-            <div className="flex items-center justify-between">
-              <p className="heading !pb-0">Đăng ký</p>
-              <span
-                className="cursor-pointer"
-                onClick={() => setOpenModal(false)}
-              >
-                <i className="fas fa-times text-[#adadad] hover:text-red text-[20px]"></i>
-              </span>
-            </div>
-          }
-          actions={[
-            <div className="link" onClick={() => setOpenModal("signIn")}>
-              <a className="!mt-0 !inline-block">Đăng nhập</a>
-            </div>,
-            <div
-              className="link"
-              onClick={() => setOpenModal("forgetPassword")}
-            >
-              <a className="!mt-0 !inline-block">Quên mật khẩu?</a>
-            </div>,
-          ]}
-        >
-          <form onSubmit={handleSubmit(_onPress)}>
-            <div className="col-span-2 md:col-span-1">
-              <FormInput
-                homeType="register"
-                label="Tên đăng nhập"
-                control={control}
-                placeholder="Nhập UserName"
-                name="UserName"
-                rules={{
-                  required: "Vui lòng điền username.",
-                  minLength: {
-                    value: 6,
-                    message: "Tôi thiểu 6 kí tự!",
-                  },
-                  maxLength: {
-                    value: 30,
-                    message: "Tối đa 30 kí tự",
-                  },
-                  validate: {
-                    check: (value) => {
-                      const check = _format.checkUserNameVNese(value.trim());
-                      if (value.trim().includes(" ")) {
-                        return "Không chứa khoảng trắng giữa 2 chữ!";
-                      }
-                      if (check) {
-                        return "Không được chứa Tiếng Việt!";
-                      }
-                      return checkUnique(value.trim(), EUnique.username);
-                    },
-                  },
-                }}
-                type={"text"}
-                disabled={isLoading}
-                prefix={<i className="fas fa-user"></i>}
-              />
-            </div>
-            <div className="col-span-2 md:col-span-1">
-              <FormInput
-                control={control}
-                homeType="register"
-                label="Họ & tên"
-                placeholder="Nhập họ & tên"
-                name="FullName"
-                type={"text"}
-                rules={{
-                  required: "Vui lòng điền thông tin",
-                }}
-                disabled={isLoading}
-                prefix={<i className="fas fa-user-tag"></i>}
-              />
-            </div>
-            <div className="col-span-2 md:col-span-1">
-              <FormInput
-                control={control}
-                homeType="register"
-                label="Email"
-                placeholder="Nhập địa chỉ email"
-                name="Email"
-                disabled={isLoading}
-                type={"email"}
-                rules={{
-                  required: "Vui lòng điền email.",
-                  pattern: {
-                    value: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
-                    message: "Không đúng định dạng!",
-                  },
-                  validate: {
-                    check: (value) => {
-                      return checkUnique(value.trim(), EUnique.email);
-                    },
-                  },
-                }}
-                prefix={<i className="fas fa-envelope"></i>}
-              />
-            </div>
-            <div className="col-span-2 md:col-span-1">
-              <FormInput
-                homeType="register"
-                label="Số điện thoại"
-                control={control}
-                placeholder="Nhập số điện thoại"
-                name="Phone"
-                disabled={isLoading}
-                prefix={<i className="fas fa-phone-plus"></i>}
-                rules={{
-                  required: "Nhập số điện thoại.",
-                  minLength: {
-                    value: 10,
-                    message: "Tối thiểu 10 số!",
-                  },
-                  pattern: {
-                    value:
-                      /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/,
-                    message: "Sđt không đúng định dạng!",
-                  },
-                  validate: {
-                    check: (value) => {
-                      return checkUnique(value.trim(), EUnique.phone);
-                    },
-                  },
-                }}
-              />
-            </div>
-            <div className="col-span-2 md:col-span-1">
-              <FormInput
-                homeType="register"
-                label="Mật khẩu"
-                prefix={<i className="fas fa-lock"></i>}
-                control={control}
-                placeholder="Nhập mật khẩu"
-                name="Password"
-                disabled={isLoading}
-                suffix={
-                  <div className="show-pass" onClick={() => setPsIcon(!psIcon)}>
-                    <i
-                      className={!psIcon ? "fas fa-eye-slash" : "fas fa-eye"}
-                    ></i>
-                  </div>
-                }
-                type={!psIcon ? "password" : "text"}
-                rules={{
-                  minLength: {
-                    value: 8,
-                    message: "Tối thiểu 8 kí tự!",
-                  },
-                  validate: {
-                    check: (value) => {
-                      const check = _format.checkUserNameVNese(value.trim());
+    <div className="authContainer">
+      <form onSubmit={handleSubmit(_onPress)}>
+        <div className="col-span-2 md:col-span-1">
+          <FormInput
+            homeType="register"
+            label="Tên đăng nhập"
+            control={control}
+            placeholder="Nhập UserName"
+            name="UserName"
+            rules={{
+              required: "Vui lòng điền username.",
+              minLength: {
+                value: 6,
+                message: "Tôi thiểu 6 kí tự!",
+              },
+              maxLength: {
+                value: 30,
+                message: "Tối đa 30 kí tự",
+              },
+              validate: {
+                check: (value) => {
+                  const check = _format.checkUserNameVNese(value.trim());
+                  if (value.trim().includes(" ")) {
+                    return "Không chứa khoảng trắng giữa 2 chữ!";
+                  }
+                  if (check) {
+                    return "Không được chứa Tiếng Việt!";
+                  }
+                  return checkUnique(value.trim(), EUnique.username);
+                },
+              },
+            }}
+            type={"text"}
+            disabled={isLoading}
+            prefix={<i className="fas fa-user"></i>}
+          />
+        </div>
+        <div className="col-span-2 md:col-span-1">
+          <FormInput
+            control={control}
+            homeType="register"
+            label="Họ & tên"
+            placeholder="Nhập họ & tên"
+            name="FullName"
+            type={"text"}
+            rules={{
+              required: "Vui lòng điền thông tin",
+            }}
+            disabled={isLoading}
+            prefix={<i className="fas fa-user-tag"></i>}
+          />
+        </div>
+        <div className="col-span-2 md:col-span-1">
+          <FormInput
+            control={control}
+            homeType="register"
+            label="Email"
+            placeholder="Nhập địa chỉ email"
+            name="Email"
+            disabled={isLoading}
+            type={"email"}
+            rules={{
+              required: "Vui lòng điền email.",
+              pattern: {
+                value: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+                message: "Không đúng định dạng!",
+              },
+              validate: {
+                check: (value) => {
+                  return checkUnique(value.trim(), EUnique.email);
+                },
+              },
+            }}
+            prefix={<i className="fas fa-envelope"></i>}
+          />
+        </div>
+        <div className="col-span-2 md:col-span-1">
+          <FormInput
+            homeType="register"
+            label="Số điện thoại"
+            control={control}
+            placeholder="Nhập số điện thoại"
+            name="Phone"
+            disabled={isLoading}
+            prefix={<i className="fas fa-phone-plus"></i>}
+            rules={{
+              required: "Nhập số điện thoại.",
+              minLength: {
+                value: 10,
+                message: "Tối thiểu 10 số!",
+              },
+              pattern: {
+                value:
+                  /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/,
+                message: "Sđt không đúng định dạng!",
+              },
+              validate: {
+                check: (value) => {
+                  return checkUnique(value.trim(), EUnique.phone);
+                },
+              },
+            }}
+          />
+        </div>
+        <div className="col-span-2 md:col-span-1">
+          <FormInput
+            homeType="register"
+            label="Mật khẩu"
+            prefix={<i className="fas fa-lock"></i>}
+            control={control}
+            placeholder="Nhập mật khẩu"
+            name="Password"
+            disabled={isLoading}
+            suffix={
+              <div className="show-pass" onClick={() => setPsIcon(!psIcon)}>
+                <i className={!psIcon ? "fas fa-eye-slash" : "fas fa-eye"}></i>
+              </div>
+            }
+            type={!psIcon ? "password" : "text"}
+            rules={{
+              minLength: {
+                value: 8,
+                message: "Tối thiểu 8 kí tự!",
+              },
+              validate: {
+                check: (value) => {
+                  const check = _format.checkUserNameVNese(value.trim());
 
-                      if (value.trim() === "") {
-                        return "Nhập mật khẩu.";
-                      }
+                  if (value.trim() === "") {
+                    return "Nhập mật khẩu.";
+                  }
 
-                      if (value.trim().includes(" ")) {
-                        return "Không chứa khoảng trắng giữa 2 chữ!";
-                      }
-                      if (check) {
-                        return "Không được chứa Tiếng Việt!";
-                      }
-                    },
-                  },
-                }}
-              />
-            </div>
-            <div className="col-span-2 md:col-span-1">
-              <FormInput
-                control={control}
-                homeType="register"
-                label="Nhập lại mật khẩu"
-                placeholder="Nhập lại mật khẩu"
-                name="ConfirmPassword"
-                disabled={isLoading}
-                prefix={<i className="fas fa-lock"></i>}
-                suffix={
-                  <div
-                    className="show-pass"
-                    onClick={() => setCpsIcon(!cpsIcon)}
-                  >
-                    <i
-                      className={!cpsIcon ? "fas fa-eye-slash" : "fas fa-eye"}
-                    ></i>
-                  </div>
-                }
-                type={!cpsIcon ? "password" : "text"}
-                rules={{
-                  required: "Xác nhận lại mật khẩu.",
-                  validate: {
-                    checkEqualPassword: (value) => {
-                      const check = _format.checkUserNameVNese(value.trim());
+                  if (value.trim().includes(" ")) {
+                    return "Không chứa khoảng trắng giữa 2 chữ!";
+                  }
+                  if (check) {
+                    return "Không được chứa Tiếng Việt!";
+                  }
+                },
+              },
+            }}
+          />
+        </div>
+        <div className="col-span-2 md:col-span-1">
+          <FormInput
+            control={control}
+            homeType="register"
+            label="Nhập lại mật khẩu"
+            placeholder="Nhập lại mật khẩu"
+            name="ConfirmPassword"
+            disabled={isLoading}
+            prefix={<i className="fas fa-lock"></i>}
+            suffix={
+              <div className="show-pass" onClick={() => setCpsIcon(!cpsIcon)}>
+                <i className={!cpsIcon ? "fas fa-eye-slash" : "fas fa-eye"}></i>
+              </div>
+            }
+            type={!cpsIcon ? "password" : "text"}
+            rules={{
+              required: "Xác nhận lại mật khẩu.",
+              validate: {
+                checkEqualPassword: (value) => {
+                  const check = _format.checkUserNameVNese(value.trim());
 
-                      if (value.trim() === "") {
-                        return "Nhập mật khẩu";
-                      }
+                  if (value.trim() === "") {
+                    return "Nhập mật khẩu";
+                  }
 
-                      if (value.trim().includes(" ")) {
-                        return "Không chứa khoảng trắng giữa 2 chữ!";
-                      }
-                      if (check) {
-                        return "Không được chứa Tiếng Việt!";
-                      }
-                      return password === value.trim() || createComplain();
-                    },
-                  },
-                }}
-              />
-            </div>
-            <div className="col-span-2">
-              <Button
-                loading={loading}
-                title="Đăng ký"
-                btnClass="w-full"
-                htmlType="submit"
-              />
-            </div>
-          </form>
-        </Card>
+                  if (value.trim().includes(" ")) {
+                    return "Không chứa khoảng trắng giữa 2 chữ!";
+                  }
+                  if (check) {
+                    return "Không được chứa Tiếng Việt!";
+                  }
+                  return password === value.trim() || createComplain();
+                },
+              },
+            }}
+          />
+        </div>
+        <div className="col-span-2">
+          <Button
+            loading={loading}
+            title="Đăng ký"
+            btnClass="w-full"
+            htmlType="submit"
+          />
+        </div>
+      </form>
+
+      <div className="py-4 flex justify-between">
+        <span className={aLink} onClick={() => handleOpen("login")}>
+          Đăng nhập hệ thống
+        </span>
+        <span className={aLink} onClick={() => handleOpen("forgetPass")}>
+          Quên mật khẩu?
+        </span>
       </div>
-    </Modal>
+    </div>
   );
 };
 
-export const RegisterFormMemo = React.memo(RegisterForm)
+export const RegisterFormMemo = React.memo(RegisterForm);
---
+++ src/pages/authen/login.tsx
@@ -0,0 +1,70 @@
+import { useSelector } from "react-redux";
+import BlankLayout from "~/components/globals/layout/blankLayouts";
+import { RootState } from "~/store";
+import { TNextPageWithLayout } from "~/types/layout";
+import styles from "./index.module.css";
+import { Divider } from "antd";
+import { ForgotPasswordForm, RegisterFormMemo, SignInForm } from "~/components";
+import { useCallback, useState } from "react";
+import { HomeLayoutProtector } from "~/components/globals/layout/homeLayouts/HomeLayoutProtector";
+
+const Index: TNextPageWithLayout = () => {
+  const dataGlobal: TConfig = useSelector(
+    (state: RootState) => state.dataGlobal
+  );
+
+  const [isOpen, setIsOpen] = useState("login");
+
+  const handleSetOpen = useCallback((x) => setIsOpen(x), []);
+
+  return (
+    <HomeLayoutProtector>
+      <div className={styles.loginPage}>
+        <div className={styles.loginWrapper}>
+          <div>
+            <div className={styles.loginText}>
+              <div className={styles.loginLogo}>
+                <img src={dataGlobal?.LogoIMG} alt="" />
+              </div>
+              <Divider />
+              {isOpen === "login" && <h1>Đăng nhập hệ thống</h1>}
+
+              {isOpen === "register" && <h1>Đăng ký tài khoản</h1>}
+
+              {isOpen === "forgetPass" && <h1>Tìm lại mật khẩu</h1>}
+            </div>
+          </div>
+          {isOpen === "login" && <SignInForm handleOpen={handleSetOpen} />}
+
+          {isOpen === "register" && (
+            <RegisterFormMemo handleOpen={handleSetOpen} />
+          )}
+
+          {isOpen === "forgetPass" && (
+            <ForgotPasswordForm handleOpen={handleSetOpen} />
+          )}
+
+          <div className={styles.loginContact}>
+            <div className={styles.infoWrapper}>
+              <span className={styles.infoLabel}>Hotline: </span>
+              <span className={styles.infoValue}>{dataGlobal?.Hotline}</span>
+            </div>
+            <div className={styles.infoWrapper}>
+              <span className={styles.infoLabel}>Email: </span>
+              <span className={styles.infoValue}>
+                {dataGlobal?.EmailContact}
+              </span>
+            </div>
+          </div>
+        </div>
+        <img src="/default/login-bottom.png" alt="" className={styles.img} />
+      </div>
+    </HomeLayoutProtector>
+  );
+};
+
+// Index.displayName = SEOConfigs.homePage;
+Index.Layout = BlankLayout;
+Index.displayName = "Hệ thống";
+
+export default Index;
