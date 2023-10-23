b5a0897828ea937ed4d0dfb6f3ce139268b08e66
Hieu Liem <hieuliem33@gmail.com>
2023-10-23 11:24:18 +0700

    hot-fix-wrong-name-folder-path-2

---
+++ src/components/screens/user/searchProduct/detailPage/layoutRight/components/common/renderPriceOrigin.tsx
@@ -0,0 +1,52 @@
+import { _format } from "~/utils";
+import styles from "./_index.module.scss";
+
+export const RenderPriceOrigin = ({ item }) => {
+  console.log(item);
+
+  if (!item?.ConfiguredItems?.length) {
+    /// not yet check
+    return <>{_format.getYuan(item?.Price.OriginalPrice)}</>;
+  }
+  if (!!item?.Promotions?.length) {
+    const minPrice = item.Promotions[
+      item.Promotions.length - 1
+    ].ConfiguredItems.reduce((prev, cur) => {
+      if (cur.Price.OriginalPrice < prev.Price.OriginalPrice) return cur;
+      return prev;
+    });
+    const maxPrice = item.Promotions[
+      item.Promotions.length - 1
+    ].ConfiguredItems.reduce((prev, cur) => {
+      if (cur.Price.OriginalPrice > prev.Price.OriginalPrice) return cur;
+      return prev;
+    });
+    console.log(minPrice, maxPrice);
+
+    return (
+      <span>
+        {_format.getYuan(minPrice.Price.OriginalPrice)} ~{" "}
+        <span className={styles["rmb"]}></span>
+        {_format.getYuan(maxPrice.Price.OriginalPrice)}
+      </span>
+    );
+  } else {
+    const minPrice = item.ConfiguredItems.reduce((prev, cur) => {
+      if (!cur.Quantity) return prev;
+      if (cur.Price.OriginalPrice < prev.Price.OriginalPrice) return cur;
+      return prev;
+    });
+    const maxPrice = item.ConfiguredItems.reduce((prev, cur) => {
+      if (!cur.Quantity) return prev;
+      if (cur.Price.OriginalPrice > prev.Price.OriginalPrice) return cur;
+      return prev;
+    });
+    return (
+      <span>
+        {_format.getYuan(minPrice.Price.OriginalPrice)} ~{" "}
+        <span className={styles["rmb"]}></span>
+        {_format.getYuan(maxPrice.Price.OriginalPrice)}
+      </span>
+    );
+  }
+};
