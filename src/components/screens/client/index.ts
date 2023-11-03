56b4aea43e0d67dc0d6920dbbd40cbcf48f3cea7
Hieu Liem <hieuliem33@gmail.com>
2023-11-03 16:52:34 +0700

    refeat/remove-manage-client-create-order

--- src/components/screens/client/index.ts
+++ src/components/screens/client/index.ts
@@ -1,6 +1,5 @@
 export * from "./OrderListClientTable";
 export * from "./OrderListClientFilter";
-export * from "./ClientCreateOrderForm";
 export * from "./ClientTransactionHistoryTable";
 export * from "./ClientTransactionHistoryFilter";
 export * from "./client-list";
