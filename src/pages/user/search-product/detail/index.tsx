3c8d0876cbc8bb945a7b4fad517a2e94f2c61268
Liêm Đặng <hieuliem33@gmail.com>
2024-01-25 14:41:54 +0700

    feat/hidden-search-API

--- src/pages/user/search-product/detail/index.tsx
+++ src/pages/user/search-product/detail/index.tsx
@@ -1,16 +1,28 @@
-import { UserLayout } from '~/components'
+// import { UserLayout } from '~/components'
+// import { TNextPageWithLayout } from '~/types/layout'
+// import DetailSearchPage from '~/components/screens/user/searchProductOneBound/detailSearchPage'
+
+// const Index: TNextPageWithLayout = () => {
+//   return (
+//     <div className='my-[24px]'>
+//       <DetailSearchPage />
+//     </div>
+//   )
+// }
+
+// Index.displayName = 'Tìm kiếm sản phẩm'
+// Index.Layout = UserLayout
+
+// export default Index
+import React from 'react'
+import { NotFound } from '~/components'
+import BlankLayout from '~/components/globals/layout/blankLayouts'
 import { TNextPageWithLayout } from '~/types/layout'
-import DetailSearchPage from '~/components/screens/user/searchProductOneBound/detailSearchPage'
 
 const Index: TNextPageWithLayout = () => {
-  return (
-    <div className='my-[24px]'>
-      <DetailSearchPage />
-    </div>
-  )
+  return <NotFound />
 }
 
-Index.displayName = 'Tìm kiếm sản phẩm'
-Index.Layout = UserLayout
+Index.Layout = BlankLayout
 
 export default Index
--- src/pages/user/search-product/index.tsx
+++ src/pages/user/search-product/index.tsx
@@ -1,12 +1,24 @@
-import { UserLayout } from '~/components'
-import { SearchPage } from '~/components/screens/user/searchProductOneBound'
+// import { UserLayout } from '~/components'
+// import { SearchPage } from '~/components/screens/user/searchProductOneBound'
+// import { TNextPageWithLayout } from '~/types/layout'
+
+// const Index: TNextPageWithLayout = () => {
+//   return <SearchPage />
+// }
+
+// Index.displayName = 'Tìm kiếm sản phẩm'
+// Index.Layout = UserLayout
+
+// export default Index
+import React from 'react'
+import { NotFound } from '~/components'
+import BlankLayout from '~/components/globals/layout/blankLayouts'
 import { TNextPageWithLayout } from '~/types/layout'
 
 const Index: TNextPageWithLayout = () => {
-  return <SearchPage />
+  return <NotFound />
 }
 
-Index.displayName = 'Tìm kiếm sản phẩm'
-Index.Layout = UserLayout
+Index.Layout = BlankLayout
 
 export default Index
